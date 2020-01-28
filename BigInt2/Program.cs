d99ad2f1a42161c710a3783190288e010c5a670f
chrizz92 <christianschadler@gmx.at>
2020-01-28 20:08:22 +0100

    Ergänzung Methoden

--- BigInt2/Program.cs
+++ BigInt2/Program.cs
@@ -58,18 +58,43 @@ namespace BigInt2
         static string AddBigInts(string number1, string number2)
         {
             string result = "";
+            string temp = "";
+            int digit1, digit2, nextDigit = 0;
 
+            //Falls Number1 und Number2 unterschiedlich lang sind
             if(number1.Length != number2.Length)
             {
                 int timesToAddZero = number1.Length - number2.Length;
                 if (timesToAddZero<0)
                 {
                     timesToAddZero *= -1;
+                    number1 = AddLeadingChars(number1, "0", timesToAddZero);
                 }
                 else
                 {
-
+                    number2 = AddLeadingChars(number2, "0", timesToAddZero);
                 }
+            }
+
+            for (int i = number2.Length; i >= 0; i--)
+            {
+                digit1 = Convert.ToInt32(number1[i]);
+                digit2 = Convert.ToInt32(number2[i]);
+                digit1 += digit2;
+
+                if (digit1 > 9)
+                {
+                    nextDigit = 1;
+                }
+                else
+                {
+                    digit1 += nextDigit;
+                    nextDigit = 0;
+                }
+
+                digit1 %= 10;
+
+                result = digit1.ToString() + result;
             }
 
             return result;
@@ -79,15 +104,16 @@ namespace BigInt2
 
         static void Main(string[] args)
         {
-            string number1, number2;
+            string number1, number2, result;
 
             Console.WriteLine("BIG-INT");
             Console.WriteLine("=======");
 
             number1 = ReadBigNumber(1);
             number2 = ReadBigNumber(2);
-
-
+            result = AddBigInts(number1, number2);
+            Console.WriteLine("Summe der Zahlen:");
+            Console.WriteLine(result);
         }
     }
 }
